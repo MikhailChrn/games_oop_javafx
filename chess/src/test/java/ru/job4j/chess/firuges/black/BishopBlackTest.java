7c94cc7b052de2f2469e569636a3d2bb34a46027
MikhailChrn <mikhail.cherneyev@yandex.ru>
2023-04-13 07:40:32 +0400

    3. Каркас шахматной доски [#792] Реализовать метод way() написать тест к нему

--- chess/src/test/java/ru/job4j/chess/firuges/black/BishopBlackTest.java
+++ chess/src/test/java/ru/job4j/chess/firuges/black/BishopBlackTest.java
@@ -7,8 +7,6 @@ import org.junit.jupiter.api.Test;
 import ru.job4j.chess.firuges.Cell;
 import ru.job4j.chess.firuges.Figure;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class BishopBlackTest {
 
     @Test
@@ -27,5 +25,54 @@ class BishopBlackTest {
         Figure bishopBlackAfterCopy = bishopBlackBeforeCopy.copy(destination);
         Cell result = bishopBlackAfterCopy.position();
         assertThat(destination).isEqualTo(result);
+    }
+
+    @Test
+    void isDiagonal() {
+        Cell respawn = Cell.C1;
+        Cell destination = Cell.G5;
+        BishopBlack bishopBlack = new BishopBlack(respawn);
+        boolean result = bishopBlack.isDiagonal(respawn, destination);
+        assertThat(result).isTrue();
+    }
+
+    @Test
+    void wayC1toG5() {
+        Cell respawn = Cell.C1;
+        Cell destination = Cell.G5;
+        Cell[] expected = new Cell[] {Cell.D2, Cell.E3, Cell.F4, Cell.G5};
+        Figure bishopBlack = new BishopBlack(respawn);
+        Cell[] result = bishopBlack.way(destination);
+        assertThat(result).containsExactly(expected);
+    }
+
+    @Test
+    void wayG5toC1() {
+        Cell respawn = Cell.G5;
+        Cell destination = Cell.C1;
+        Cell[] expected = new Cell[] {Cell.F4, Cell.E3, Cell.D2, Cell.C1};
+        Figure bishopBlack = new BishopBlack(respawn);
+        Cell[] result = bishopBlack.way(destination);
+        assertThat(result).containsExactly(expected);
+    }
+
+    @Test
+    void wayH8toA1() {
+        Cell respawn = Cell.H8;
+        Cell destination = Cell.A1;
+        Cell[] expected = new Cell[] {Cell.G7, Cell.F6, Cell.E5, Cell.D4, Cell.C3, Cell.B2, Cell.A1};
+        Figure bishopBlack = new BishopBlack(respawn);
+        Cell[] result = bishopBlack.way(destination);
+        assertThat(result).containsExactly(expected);
+    }
+
+    @Test
+    void wayG1toA7() {
+        Cell respawn = Cell.G1;
+        Cell destination = Cell.A7;
+        Cell[] expected = new Cell[] {Cell.F2, Cell.E3, Cell.D4, Cell.C5, Cell.B6, Cell.A7};
+        Figure bishopBlack = new BishopBlack(respawn);
+        Cell[] result = bishopBlack.way(destination);
+        assertThat(result).containsExactly(expected);
     }
 }
